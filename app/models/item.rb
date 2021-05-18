3b57610920228fd8bc120e5ae020bff066db2412
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-05-18 10:52:56 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,26 @@ class Item < ApplicationRecord
   belongs_to_active_hash :burden_id
   belongs_to_active_hash :area_id
   belongs_to_active_hash :days_delivery_id
-  
+
+  with_options presence: ture do
+    validates :image
+    validates :product_name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :days_delivery_id
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :days_delivery_id
+  end
+  validates :price, numericality: true
+
 end
