06f80e5b7cc17cd0d8f8c64d7536cffec26459e5
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-05-18 10:34:54 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category_id
+  belongs_to_active_hash :status_id
+  belongs_to_active_hash :burden_id
+  belongs_to_active_hash :area_id
+  belongs_to_active_hash :days_delivery_id
+  
 end
