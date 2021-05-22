8fd0dec744fed019cb6c12e3b74de2a987ddff1b
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-05-22 23:22:21 +0900

    attr_accessorの追加

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -1,3 +1,4 @@
 class PurchaseDestination
-  
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :purchase_id, :postal_code, :area_id, :municipality, :address, :building_name, :phone_number, :token
 end
