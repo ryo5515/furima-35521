811e68c3c08a4672339f17fc5be303dcf06e83e7
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-05-24 12:29:22 +0900

    purchaseモデルとのアソシエーションを設定

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,4 +3,5 @@ class Destination < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  belongs_to :purchase
 end
