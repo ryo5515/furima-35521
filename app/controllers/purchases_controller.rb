a84043f2d3addcb9842f112f726bb6dd4756d6a3
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-05-24 15:55:03 +0900

    ビューファイルが正しく表示されるよう設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,14 +1,15 @@
 class PurchasesController < ApplicationController
-  
+
   def index
+    @item = Item.find(params[:item_id])
     @purchase_destination = PurchaseDestination.new
   end
 
   private
 
-  def purchase_params
-    item = Item.find(params[:item_id])
-    params.require(:purchase_destination).permit(:postal_code, :area_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item_id)
-  end
+  # def purchase_params
+  #   item = Item.find(params[:item_id])
+  #   params.require(:purchase_destination).permit(:postal_code, :area_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item_id)
+  # end
 
 end
