9e30839a8339bd84502ece44f0992e959e089751
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-05-24 12:32:00 +0900

    purchaseモデルでのアソシエーションの設定

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,4 +3,7 @@ class Purchase < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  belongs_to :user
+  belongs_to :item
+  has_one :destination
 end
