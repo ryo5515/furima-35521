4cbc0fc8372f572397dc6b286905250b2384db52
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-05-25 12:13:54 +0900

    購入機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,15 +1,27 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_destination = PurchaseDestination.new
+  end
+
+  def create
+    @purchase_destination = PurchaseDestination.new(purchase_params)
+    if @purchase_destination.valid?
+      @purchase_destination.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
-  # def purchase_params
-  #   item = Item.find(params[:item_id])
-  #   params.require(:purchase_destination).permit(:postal_code, :area_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item_id)
-  # end
+  def purchase_params
+    params.require(:purchase_destination).permit(:postal_code, :area_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end 
 end
--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -1,9 +1,8 @@
 class PurchaseDestination
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :purchase_id, :postal_code, :area_id, :municipality, :address, :building_name, :phone_number, :token
+  attr_accessor :user_id, :item_id, :purchase_id, :postal_code, :area_id, :municipality, :address, :building_name, :phone_number
 
   with_options presence: true do
-    validates :token
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
@@ -15,6 +14,6 @@ class PurchaseDestination
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    destination = Destination.create(postal_code: postal_code, area_id: area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order_id)
+    Destination.create(postal_code: postal_code, area_id: area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase_id)
   end
 end
