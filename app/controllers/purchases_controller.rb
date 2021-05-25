6a0da5279778a6c016d6ab57bbe95b93c78a0438
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-05-25 16:52:52 +0900

    ログアウト状態のユーザーは購入ページにいけない設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,6 @@
 class PurchasesController < ApplicationController
   before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
 
   def index
     @purchase_destination = PurchaseDestination.new
