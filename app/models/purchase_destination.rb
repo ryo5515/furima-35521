4b10aaddeaa725d9314035574ae5e16b46b0a0c9
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-05-26 15:23:58 +0900

    attr_accessorの修正

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -1,6 +1,6 @@
 class PurchaseDestination
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :purchase_id, :postal_code, :area_id, :municipality, :address, :building_name, :phone_number, :token
+  attr_accessor :user_id, :item_id, :postal_code, :area_id, :municipality, :address, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :token
