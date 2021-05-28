a2c225abd036dacd9cf6a9a3ab7a588327022bda
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-05-28 12:21:57 +0900

    purchase_destinationモデルのエラーメッセージの日本語化

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -6,11 +6,11 @@ class PurchaseDestination
     validates :token
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
-    validates :area_id, numericality: { other_than: 1, message: 'Select'}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'にはハイフンを入れてください' }
+    validates :area_id, numericality: { other_than: 1, message: 'を選択してください'}
     validates :municipality
     validates :address
-    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'Input only number' }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'にはハイフンを入れずに11桁以内の数字を入力してください' }
   end
 
   def save
